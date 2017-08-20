3e2cc413207b5e1d1687f211337f667a501a025c
sivakorn <js.sivakorn02@gmail.com>
2017-08-20 15:32:03 +0700

    create propertie list controller

---
+++ src/com/spring/listMallController/PropertieListController.java
@@ -0,0 +1,19 @@
+package com.spring.listMallController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PropertieListController {
+	@RequestMapping(value = "/properties-listing", method = RequestMethod.GET)
+	public ModelAndView loadListMallPage(HttpServletRequest request, HttpSession session, Model md) {
+		ModelAndView mav = new ModelAndView("properties-listing");
+		return mav;
+	}
+}
