823a85d8f47f0da98de79e7e99d2a904a1d6ba89
WiwatHatung <thegameza39@gmail.com>
2017-09-05 17:34:40 +0700

    up jquey

--- WebContent/WEB-INF/assets/js/custom-game.js
+++ WebContent/WEB-INF/assets/js/custom-game.js
@@ -1,7 +1,25 @@
-$(function(){
-	$.getJSON('countries.json ',function(result){
-		$.each(result,function(i,country){
-			alert(country.name+" "+country.code);
+$(function() {
+	var countryOptions=null;
+	$.getJSON('js/countries.json', function(result) {
+		$.each(result, function(i, country) {
+			countryOptions += "<option value='" + country.code + "'>"
+					+ country.name + "</option>";
 		});
+		$('#country').html(countryOptions);
 	});
+	$("#country").change(
+			function() {
+				var stateOptions=null;
+				$.getJSON('js/states.json', function(result) {
+					
+					$.each(result, function(i, statename) {
+						if ($(country).val() == statename.name) {
+							stateOptions += "<option value='" +i+ statename.name
+									+ "'>" + statename.code + "</option>";
+						}
+					});
+					$('#state').html(stateOptions);
+				});
+				
+			});
 });
