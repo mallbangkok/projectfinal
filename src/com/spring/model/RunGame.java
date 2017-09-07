e5aaad7f16bd1382a1d2ceb0afcf6b49cab0390e
WiwatHatung <thegameza39@gmail.com>
2017-09-07 14:51:23 +0700

    update

--- src/com/spring/model/RunGame.java
+++ src/com/spring/model/RunGame.java
@@ -1,28 +1,8 @@
 package com.spring.model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import com.spring.addmallcontroller.MallManager;
 
 public class RunGame {
-	public static void main(String[] arge) {
-		MallManager mm = new MallManager();
-		Set<String> list = new HashSet<String>();
-		for (Mall m : mm.getAllMalls()) {
-			list.add(m.getArea());
-		}
-		String[] listStore = list.toArray(new String[list.size()]);
-		List<String> lists = new ArrayList<String>();
-		for (String s : listStore) {
-			for (Mall m : mm.getAllMalls()) {
-				if (s.equals(m.getArea())) {
-					lists.add(m.getArea() + ";;;" + m.getMallNameEng());
-				}
-			}
-		}
-	}
 
 }
