6a0fca574169d69130b9e1060ac51c9b7065f0a7
WiwatHatung <thegameza39@gmail.com>
2017-09-28 09:42:15 +0700

    update

--- src/com/spring/adminaddparking/ParkingAdminController.java
+++ src/com/spring/adminaddparking/ParkingAdminController.java
@@ -23,66 +23,34 @@ public class ParkingAdminController {
 		ModelAndView mav = new ModelAndView("admin-parking");
 		return mav;
 	}
-	//false
+
+	// true
 	@RequestMapping(value = "add-conditionIII-parking", method = RequestMethod.GET)
 	public ModelAndView addConditionIII(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("admin-parking");
-		AddMallManager mm = new AddMallManager();
-		ParkingUserManager pm = new ParkingUserManager();
+		ParkingAdminManager pm = new ParkingAdminManager();
 		String nameMall = request.getParameter("nameMalls");
 		String con1 = request.getParameter("conditionIII1");
 		String date1 = request.getParameter("dateIII1");
 		String time1 = request.getParameter("timeIII1");
 		String type1 = request.getParameter("typeIII1");
-		System.out.println("Name Mall : " + nameMall);
-		System.out.println("1 ::" + con1 + " " + date1 + " " + time1 + " " + type1);
 		String con2 = request.getParameter("conIII2");
 		String date2 = request.getParameter("dateIII2");
 		String price2 = request.getParameter("priceIII2");
-		System.out.println("2::" + con2 + " " + date2 + " " + price2);
 		String con3 = request.getParameter("conIII3");
 		String date3 = request.getParameter("dateIII3");
 		String time3 = request.getParameter("timeIII3");
 		String price3 = request.getParameter("priceIII3");
-
-		int times = Integer.parseInt(time1);
-		if ("ชั่วโมง".equals(type1)) {
-			times = (times * 60);
-
-		}
-		ConditionOfParking c1 = new ConditionOfParking(con1, times, 0, date1, "1");
-		ConditionOfParking c2 = new ConditionOfParking(con2, 0, Integer.parseInt(price2), date2, "2");
-		ConditionOfParking c3 = new ConditionOfParking(con3, Integer.parseInt(time3), Integer.parseInt(price3), date3,
-				"3");
-		Mall mall = new Mall();
-		for (Mall m : mm.getAllMalls()) {
-			if (nameMall.equalsIgnoreCase(m.getMallNameEng())) {
-				mall = m;
-			}
-		}
-		try {
-			mall.getConditionOfParking().add(c1);
-			mall.getConditionOfParking().add(c2);
-			mall.getConditionOfParking().add(c3);
-			c1.setMall(mall);
-			c2.setMall(mall);
-			c3.setMall(mall);
-			System.out.println(pm.addConditionI(c1));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println(pm.doAddConditionIII(nameMall, con1, date1, time1, type1, con2, date2, price2, con3, date3,
+				time3, price3));
 		return mav;
 	}
-	//false
+
+	// true
 	@RequestMapping(value = "add-conditionII-parking", method = RequestMethod.GET)
 	public ModelAndView addConditionII(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("admin-parking");
-
-		AddMallManager mm = new AddMallManager();
-
-		ParkingUserManager pm = new ParkingUserManager();
-
+		ParkingAdminManager pm = new ParkingAdminManager();
 		String nameMall = request.getParameter("nameMalls");
 		String conditionI = request.getParameter("nameConII1");
 		String dateI = request.getParameter("dateoftypeII1");
@@ -91,64 +59,27 @@ public class ParkingAdminController {
 		String conditionII = request.getParameter("nameConII2");
 		String dateII = request.getParameter("dateoftypeII2");
 		String price = request.getParameter("priceConII");
-
-		int times = Integer.parseInt(timeII1);
-
-		if ("ชั่วโมง".equals(timeoftypeII)) {
-			times = (times * 60);
-
-		}
-		Mall mall = new Mall();
-		ConditionOfParking c1 = new ConditionOfParking(conditionI, times, 0, dateI, "1");
-		ConditionOfParking c2 = new ConditionOfParking(conditionII, 0, Integer.parseInt(price), dateII, "2");
-		for (Mall m : mm.getAllMalls()) {
-			if (nameMall.equalsIgnoreCase(m.getMallNameEng())) {
-				mall = m;
-			}
-		}
-		try {
-			mall.getConditionOfParking().add(c1);
-			mall.getConditionOfParking().add(c2);
-			c1.setMall(mall);
-			c2.setMall(mall);
-			System.out.println(pm.addConditionI(c1));
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println(
+				pm.doAddConditionII(nameMall, conditionI, dateI, timeII1, timeoftypeII, conditionII, dateII, price));
 
 		return mav;
 	}
 
-	// false
+	// true
 	@RequestMapping(value = "add-conditionI-parking", method = RequestMethod.GET)
 	public ModelAndView addConditionI(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("admin-parking");
-
-		AddMallManager mm = new AddMallManager();
-		ParkingUserManager pm = new ParkingUserManager();
-
+		ParkingAdminManager pm = new ParkingAdminManager();
 		String conditionI = request.getParameter("typeOfFreeConditionI");
 		String dateI = request.getParameter("dateofTypeConditonI");
 		String nameMall = request.getParameter("nameMalls");
 		Mall mall = new Mall();
-		for (Mall m : mm.getAllMalls()) {
-			if (nameMall.equalsIgnoreCase(m.getMallNameEng())) {
-				mall = m;
-			}
-		}
-		ConditionOfParking c = new ConditionOfParking(conditionI, 0, 0, dateI, "1");
-		try {
-			mall.getConditionOfParking().add(c);
-			c.setMall(mall);
-			System.out.println(pm.addConditionI(c));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println(pm.doAddConditionI(nameMall, conditionI, dateI));
 
 		return mav;
 	}
-	//true
+
+	// true
 	@RequestMapping(value = "parking-admin", method = RequestMethod.GET)
 	public ModelAndView loadPageCalculateParkingAdmin(HttpSession session) {
 		ModelAndView mav = new ModelAndView("admin-parking");
@@ -156,7 +87,8 @@ public class ParkingAdminController {
 
 		return mav;
 	}
-	//true
+
+	// true
 	@RequestMapping(value = "parking-admin-typeofparking", method = RequestMethod.GET)
 	public ModelAndView loadPageTypeOfParking(HttpServletRequest request, HttpSession session, Model md) {
 		ModelAndView mav = new ModelAndView("admin-parking");
@@ -167,7 +99,7 @@ public class ParkingAdminController {
 		return mav;
 	}
 
-	//true
+	// true
 	@RequestMapping(value = "/gettype-parking", method = RequestMethod.GET)
 	public ModelAndView getType(HttpServletRequest request, HttpSession session, Model md) {
 		AddMallManager mm = new AddMallManager();
@@ -182,7 +114,8 @@ public class ParkingAdminController {
 		session.setAttribute("nameMall", null);
 		return mav;
 	}
-	//true
+
+	// true
 	@RequestMapping(value = "updateCondition", method = RequestMethod.GET)
 	public ModelAndView updateCondition(HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView("admin-parkign");
--- src/com/spring/adminaddparking/ParkingAdminManager.java
+++ src/com/spring/adminaddparking/ParkingAdminManager.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import com.spring.model.ConditionOfParking;
 import com.spring.model.HibernateConnection;
 import com.spring.model.Mall;
 
@@ -18,6 +19,91 @@ public class ParkingAdminManager {
 			}
 		}
 		return list;
+	}
+
+	public String doAddConditionIII(String nameMall, String con1, String date1, String time1, String type1, String con2,
+			String date2, String price2, String con3, String date3, String time3, String price3) {
+		String status = "";
+		int times = Integer.parseInt(time1);
+		ConditionOfParking c1 = new ConditionOfParking(con1, times, 0, date1, "1");
+		ConditionOfParking c2 = new ConditionOfParking(con2, 0, Integer.parseInt(price2), date2, "2");
+		ConditionOfParking c3 = new ConditionOfParking(con3, Integer.parseInt(time3), Integer.parseInt(price3), date3,
+				"3");
+		Mall mall = this.searchMall(nameMall);
+		try {
+			mall.getConditionOfParking().add(c1);
+			mall.getConditionOfParking().add(c2);
+			mall.getConditionOfParking().add(c3);
+			c1.setMall(mall);
+			c2.setMall(mall);
+			c3.setMall(mall);
+			System.out.println(this.addCondition(c1));
+			System.out.println(this.addCondition(c2));
+			System.out.println(this.addCondition(c3));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return status;
+	}
+
+	public String doAddConditionII(String nameMall, String conditionI, String dateI, String timeII1,
+			String timeoftypeII, String conditionII, String dateII, String price) {
+		String status = "";
+		int times = Integer.parseInt(timeII1);
+		ConditionOfParking c1 = new ConditionOfParking(conditionI, times, 0, dateI, "1");
+		ConditionOfParking c2 = new ConditionOfParking(conditionII, 0, Integer.parseInt(price), dateII, "2");
+		Mall mall = this.searchMall(nameMall);
+
+		try {
+			mall.getConditionOfParking().add(c1);
+			mall.getConditionOfParking().add(c2);
+			c1.setMall(mall);
+			c2.setMall(mall);
+			System.out.println(this.addCondition(c1));
+			System.out.println(this.addCondition(c2));
+
+			status = "Add Condition Success !";
+		} catch (Exception e) {
+			e.printStackTrace();
+			status = "Can't Add Condition  !";
+		}
+		return status;
+	}
+
+	public String doAddConditionI(String name, String conditionI, String dateI) {
+		String status = "";
+		ConditionOfParking c = new ConditionOfParking(conditionI, 0, 0, dateI, "1");
+		Mall mall = this.searchMall(name);
+		try {
+			mall.getConditionOfParking().add(c);
+			c.setMall(mall);
+			System.out.println(this.addCondition(c));
+			status = "Add Condition Success !";
+		} catch (Exception e) {
+			e.printStackTrace();
+			status = "Can't Add Condition  !";
+		}
+		return status;
+	}
+
+	public Mall searchMall(String name) {
+		Mall mall = new Mall();
+		for (Mall m : this.getAllMalls()) {
+			if (name.equalsIgnoreCase(m.getMallNameEng())) {
+				mall = m;
+			}
+		}
+		return mall;
+	}
+
+	public int checkHour(String s, int times) {
+		int time = 0;
+		if ("ชั่วโมง".equals(s)) {
+			times = (times * 60);
+		}
+		return time;
 	}
 
 	public List<Mall> getAllMalls() {
@@ -33,6 +119,7 @@ public class ParkingAdminManager {
 		}
 		return list;
 	}
+
 	public List<String> getMallType() {
 		List<String> list = new ArrayList<String>();
 		try {
@@ -45,5 +132,20 @@ public class ParkingAdminManager {
 			e.printStackTrace();
 		}
 		return list;
+	}
+
+	public String addCondition(ConditionOfParking c) {
+		try {
+			SessionFactory sessionFactory = HibernateConnection.doHibernateConnection();
+			Session session = sessionFactory.openSession();
+			session.beginTransaction();
+			session.save(c);
+			session.getTransaction().commit();
+			session.close();
+			return "Add Condition Successfully...";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Cannot Add Condition !!!";
+		}
 	}
 }
